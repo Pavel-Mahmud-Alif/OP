004a8ee8d122353a343a4824e212bbb9225547c8
BD-KING <106671590+Pavel-Mahmud-Alif@users.noreply.github.com>
2023-02-10 21:44:22 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,26 @@
-import MN
+import os, platform
+
+try:
+
+    import requests,time,sleep
+
+except:
+
+    os.system('pip install requests')
+    os.system('git pull')
+    os.system(' pip uninstall requests chardet urllib3 idna certifi -y ')
+    os.system('pip install chardet urllib3 idna certifi requests')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    import MN
+
+
+elif bit == '32bit':
+
+    os.system('python MN.py')
+    time.sleep(1000)
